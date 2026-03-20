b040c01a39ce60612366d2491569a61ffffc8af4
Stratos Thivaios <me@sthivaios.dev>
2026-03-20 22:54:24 +0200

    cli: rootcmd description and desclaimer footer

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -1,31 +1,51 @@
 /*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+ODSCI CLI - An STM32-based USB interface for DS18B20 temperature sensors
+# Copyright (C) 2026  Stratos Thivaios
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+
 package cmd
 
 import (
+	_ "embed"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
+var desclaimer = `
+---------------------------------------------------------------------
+ODSCI CLI - Copyright (C) 2026  Stratos Thivaios
+This program comes with ABSOLUTELY NO WARRANTY; for details type 'show w'.
+This is free software, and you are welcome to redistribute it under certain
+conditions; type 'show c' for details.
+---------------------------------------------------------------------
+`
+
 var rootCmd = &cobra.Command{
 	Use:   "odsci",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "The ODSCI CLI",
+	Long: `The ODSCI CLI is the official command line application
+used to interface with ODSCI probes. Learn more about ODSCI
+at https://github.com/sthivaios/odsci.
+ 
+Begin by connecting your probe to your computer.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -34,15 +54,7 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.odsci.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.SetUsageTemplate(rootCmd.UsageTemplate() + desclaimer)
 }
 
 
