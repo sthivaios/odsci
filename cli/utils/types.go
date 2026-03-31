c1615d3d8625de586582e9be4705847b74049a89
Stratos Thivaios <me@sthivaios.dev>
2026-03-31 03:25:28 +0300

    cli: board info check function

--- cli/utils/types.go
+++ cli/utils/types.go
@@ -1,6 +1,29 @@
+/*
+ODSCI CLI - An STM32-based USB interface for DS18B20 temperature sensors
+# Copyright (C) 2026  Stratos Thivaios
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package utils
 
 type Sample struct {
 	Timestamp int64
 	Value float64
+}
+
+type BoardInfo struct {
+	CledIsUsedForErrors bool
+	FirmwareVersion string
 }
