8efc507bc3b66211f250c32f808affb6082411ee
Stratos Thivaios <me@sthivaios.dev>
2026-04-06 06:56:07 +0300

    firmware: USB descriptors

--- firmware/USB_DEVICE/App/usbd_desc.c
+++ firmware/USB_DEVICE/App/usbd_desc.c
@@ -64,7 +64,7 @@
 
 #define USBD_VID     1155
 #define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "sthivaios.dev"
+#define USBD_MANUFACTURER_STRING     "Stratos Thivaios"
 #define USBD_PID_FS     0x5740
 #define USBD_PRODUCT_STRING_FS     "ODSCI v1 Probe"
 #define USBD_CONFIGURATION_STRING_FS     "ODSCI Configuration"
